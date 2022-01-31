2a7248837f7762d52f86a210ecab13a75ecd13b9
Jordan <Berti@epse.be>
2022-01-31 09:57:37 +0100

    cours 9 - listings et routage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+const HomePage = (props) => {
+    return ( 
+        <div className="jumbotron">
+            <h1 className="display-3">Hello world!</h1>
+            <p className="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur ducimus aliquid, magnam quaerat hic voluptas?</p>
+            <hr className="my-4" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, doloremque!</p>
+            <p className="lead">
+                <a href="#" className="btn btn-primary btn-lg">Learn More</a>
+            </p>
+        </div>
+     );
+}
+ 
+export default HomePage;
